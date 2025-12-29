bc67120d1f5669e5be9fa220ba14caf6118c72af
mario4off <mario31991@gmail.com>
2025-12-29 21:02:44 +0100

    Refactor: Remove unused components and UI elements; update sign-up and password forms
    - Removed the ThemeSwitcher, Badge, Button, Card, Checkbox, DropdownMenu, Input, Label components.
    - Deleted the sign-up form and update password form implementations.
    - Added a new logo SVG file.
    - Commented out the SignUpForm implementation for potential future use.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { ThemeProvider } from "next-themes";
 import { poppins } from "@/components/ui/fonts";
 import "./globals.css";
 import Image from "next/image";
@@ -26,24 +25,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${poppins.className} antialiased`}>
-        <header className="h-24 w-full bg-black z-10 flex items-center px-4 shadow-2xl">
-          <Image
-            src="/devslug_logo.svg"
-            width={250}
-            height={250}
-            className="mx-4 "
-            alt="Logo devslug"
-          />
-        </header>
-        <Suspense fallback={<div>Cargando...</div>}>
+      <body className={`${poppins.className} antialiased bg-black`}>
+        <Suspense fallback={<div></div>}>
           <Vortex
             backgroundColor="black"
-            rangeY={800}
-            particleCount={250}
+            rangeY={400}
+            particleCount={230}
             baseHue={120}
-            className=""
+            className="min-h-screen"
           >
+            <header className="w-full bg-gradient-to-b from-black z-10 flex items-center  mx-4 shadow-2xl">
+              <Image
+                src="/logo.svg"
+                width={250}
+                height={95}
+                loading="eager"
+                alt="Logo devslug"
+              />
+            </header>
             {children}
           </Vortex>
         </Suspense>
--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,5 @@ import Link from "next/link";
 import { Suspense } from "react";
 
 export default function Home() {
-  return <main className="min-h-screen flex flex-col items-center"></main>;
+  return <main className=""></main>;
 }
