f7f01bf22bdb55617711f65aea85870e38e7cb5f
mario4off <mario31991@gmail.com>
2026-02-15 22:55:13 +0100

    next try

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  reactStrictMode: true,
+  env: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  },
 };
 
 export default nextConfig;
