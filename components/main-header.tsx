983c45a1adbe3af81462fb5b9187ce1b26bb303b
mario4off <mario31991@gmail.com>
2026-03-08 19:58:51 +0100

    refactor MainHeader and add MobileMenu component for improved mobile navigation

--- components/main-header.tsx
+++ components/main-header.tsx
@@ -2,15 +2,7 @@ import Image from "next/image";
 import { auth } from "@/auth";
 import SignInButton from "./sign-in-button";
 import SignOutButton from "./sign-out-button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import MobileMenu from "./mobile-menu";
 
 export default async function MainHeader() {
   const session = await auth();
@@ -30,39 +22,9 @@ export default async function MainHeader() {
       {!isLogged ? (
         <SignInButton className="hidden md:flex" />
       ) : (
-        <>
-          <SignOutButton className="hidden md:flex" />
-          <div className="block md:hidden">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="35"
-                  height="35"
-                  viewBox="0 0 24 24"
-                  fill="#ffff"
-                  className="icon icon-tabler icons-tabler-filled icon-tabler-menu-2"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M21 6a1 1 0 0 1 -1 1h-16a1 1 0 1 1 0 -2h16a1 1 0 0 1 1 1" />
-                  <path d="M21 12a1 1 0 0 1 -1 1h-16a1 1 0 0 1 0 -2h16a1 1 0 0 1 1 1" />
-                  <path d="M21 18a1 1 0 0 1 -1 1h-16a1 1 0 0 1 0 -2h16a1 1 0 0 1 1 1" />
-                </svg>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuGroup>
-                  <DropdownMenuLabel>Mi Cuenta</DropdownMenuLabel>
-                  <DropdownMenuItem>Perfil</DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem>
-                    <SignOutButton className="" />
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-        </>
+        <SignOutButton className="hidden md:flex" />
       )}
+      <MobileMenu isLogged={isLogged} />
     </header>
   );
 }
---
+++ components/mobile-menu.tsx
@@ -0,0 +1,62 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import SignOutButton from "./sign-out-button";
+
+export default function MobileMenu({
+  isLogged = false,
+}: {
+  isLogged: boolean;
+}) {
+  return !isLogged ? (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="35"
+      height="35"
+      viewBox="0 0 24 24"
+      fill="#1F2937"
+      className="block md:hidden icon icon-tabler icons-tabler-filled icon-tabler-menu-2"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M21 6a1 1 0 0 1 -1 1h-16a1 1 0 1 1 0 -2h16a1 1 0 0 1 1 1" />
+      <path d="M21 12a1 1 0 0 1 -1 1h-16a1 1 0 0 1 0 -2h16a1 1 0 0 1 1 1" />
+      <path d="M21 18a1 1 0 0 1 -1 1h-16a1 1 0 0 1 0 -2h16a1 1 0 0 1 1 1" />
+    </svg>
+  ) : (
+    <div className="block md:hidden">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="35"
+            viewBox="0 0 24 24"
+            fill="#ffff"
+            className="icon icon-tabler icons-tabler-filled icon-tabler-menu-2"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M21 6a1 1 0 0 1 -1 1h-16a1 1 0 1 1 0 -2h16a1 1 0 0 1 1 1" />
+            <path d="M21 12a1 1 0 0 1 -1 1h-16a1 1 0 0 1 0 -2h16a1 1 0 0 1 1 1" />
+            <path d="M21 18a1 1 0 0 1 -1 1h-16a1 1 0 0 1 0 -2h16a1 1 0 0 1 1 1" />
+          </svg>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuGroup>
+            <DropdownMenuLabel>Mi Cuenta</DropdownMenuLabel>
+            <DropdownMenuItem>Perfil</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <SignOutButton className="" />
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
