d065324afef150ee96515d1b9df6f70f0c6aa086
mario4off <mario31991@gmail.com>
2026-05-14 19:24:37 +0200

    feat: create validation urlschema with zod and installation of zod

--- lib/actions/url.ts
+++ lib/actions/url.ts
@@ -1,5 +1,13 @@
 "use server";
 
+import { urlSchema } from "@/validations/urlSchema";
+
 export async function insertUrl(formdata: FormData) {
+  const url = formdata.get("url") as string;
+
+  const result = urlSchema.safeParse({ url });
+
+  console.log(result);
+
   console.log("Guardamos la url en la base de datos");
 }
---
+++ validations/urlSchema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const urlSchema = z.object({
+  url: z
+    .string()
+    .min(3, { message: "La URL debe tener al menos 3 caracteres de longitud." })
+    .max(250, {
+      message: "La URL no puede exceder 250 caracteres de longitud.",
+    }),
+});
