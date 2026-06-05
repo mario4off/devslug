c3b40f2b171c3c09e80a00672d7e3aad08983947
mario4off <mario31991@gmail.com>
2026-06-05 20:41:06 +0200

    fix: modify  zod's urlschema for allowing userId as optional value

--- validations/url.ts
+++ validations/url.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const urlFormSchema = z.object({
   url: z.url(),
   slug: z.string().length(7),
-  userId: z.int().positive,
+  userId: z.int().positive().optional(),
 });
 
 export type UrlFormState = {
