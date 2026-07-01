3d0e3079d42e6275427fb32ee702617d13b6f2c4
mario4off <mario31991@gmail.com>
2026-07-01 19:23:37 +0200

    refactor: modify imports for prisma connection

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "@/lib/db/db";
+import prisma from "@/lib/db";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
