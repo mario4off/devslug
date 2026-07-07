0c77a437214aa6098546689613c70d3b411d17e9
mario4off <mario31991@gmail.com>
2026-07-07 19:13:56 +0200

    style: update PrimaryButton component to handle disabled state and adjust styling accordingly

--- components/ui/primaryButton.tsx
+++ components/ui/primaryButton.tsx
@@ -1,14 +1,21 @@
 function PrimaryButton({
   title,
   type = "button",
+  disabled = true,
 }: {
   title: string;
   type?: "submit" | "button";
+  disabled?: boolean;
 }) {
   return (
     <button
       type={type}
-      className="bg-slate-950 border-teal-400 border text-white p-3 rounded-md w-fit hover:bg-teal-950 hover:scale-105 transition duration-300 ease-in-out "
+      disabled={disabled}
+      className={
+        disabled
+          ? "bg-gray-900 border-slate-800 border text-gray-400 p-3 rounded-md w-fit"
+          : `bg-slate-950 border-teal-400 border text-white p-3 rounded-md w-fit hover:bg-teal-950 hover:scale-105 transition duration-300 ease-in-out `
+      }
     >
       {title}
     </button>
