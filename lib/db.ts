a130ec5d4ec883bd0fd84e9b8fb08f48ba0d12fd
mario4off <mario31991@gmail.com>
2026-07-28 19:38:36 +0200

    fix: db

--- lib/db.ts
+++ lib/db.ts
@@ -1,20 +1,19 @@
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+const prismaClientSingleton = () => {
+  const adapter = new PrismaPg(process.env.DATABASE_URL!);
+  return new PrismaClient({ adapter });
 };
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-  });
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const db = globalForPrisma.prisma ?? prismaClientSingleton();
 
-export default prisma;
+export default db;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
