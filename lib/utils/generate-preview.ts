41a8e5138aad6c66b2f20606160560a6af5e7766
mario4off <mario31991@gmail.com>
2026-08-04 18:44:08 +0200

    fix: handle optional data fields in meta tags

--- lib/utils/generate-preview.ts
+++ lib/utils/generate-preview.ts
@@ -6,15 +6,15 @@ export default function generatePreview(data: UrlMeta) {
   <html>
     <head>
          <title>${data.title}</title>
-        <meta property="og:title" content="${data.title}">
-        <meta property="og:description" content="${data.description}">
-        <meta property="og:image" content="${data.image}">
-        <link rel="icon" href="${data.favicon}">
+        <meta property="og:title" content="${data.title ?? ""}">
+        <meta property="og:description" content="${data.description ?? ""}">
+        <meta property="og:image" content="${data.image ?? ""}">
+        <link rel="icon" href="${data.favicon ?? ""}">
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image">
-        <meta name="twitter:title" content="${data.title}">
-        <meta name="twitter:description" content="${data.description}">
-        <meta name="twitter:image" content="${data.image}">
+        <meta name="twitter:title" content="${data.title ?? ""}">
+        <meta name="twitter:description" content="${data.description ?? ""}">
+        <meta name="twitter:image" content="${data.image ?? ""}">
     </head>
     <body></body>
     </html>`;
