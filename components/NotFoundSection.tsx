ffa81a679023b70778896a2e99717527d22d876d
mario4off <mario31991@gmail.com>
2026-08-11 20:37:32 +0200

    style: improve layout of NotFoundSection by adjusting text width

--- components/NotFoundSection.tsx
+++ components/NotFoundSection.tsx
@@ -10,7 +10,7 @@ export default function NotFoundSection() {
         <h1 className={`${sora.className} text-center`}>
           No encontramos este enlace
         </h1>
-        <p className="mt-3 text-center text-base md:text-xl">
+        <p className="mt-3 text-center text-base md:text-xl w-3/4">
           El enlace que estás buscando no existe o ya no está disponible.
         </p>
         <Image
@@ -21,7 +21,7 @@ export default function NotFoundSection() {
         ></Image>
       </header>
       <footer className=" flex mt-8 flex-col items-center gap-8">
-        <h2 className="text-center">Acorta una nueva URL en dos clics</h2>
+        <h2 className="text-center w-3/4">Acorta una nueva URL en dos clics</h2>
         <Link href="/">
           <PrimaryButton title="Acortar nueva URL" disabled={false} />
         </Link>
