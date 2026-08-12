85a0a18aa8d99d22e0ea51c0ae5ea861f2d685ef
mario4off <mario31991@gmail.com>
2026-08-12 09:04:03 +0200

    style: adjust width of NotFoundSection for improved layout

--- components/NotFoundSection.tsx
+++ components/NotFoundSection.tsx
@@ -5,12 +5,12 @@ import PrimaryButton from "./ui/PrimaryButton";
 
 export default function NotFoundSection() {
   return (
-    <section className=" mt-5">
+    <section className=" mt-5 w-5/6 ">
       <header className="flex flex-col items-center">
         <h1 className={`${sora.className} text-center`}>
           No encontramos este enlace
         </h1>
-        <p className="mt-3 text-center text-base md:text-xl w-3/4">
+        <p className="mt-3 text-center text-base md:text-xl ">
           El enlace que estás buscando no existe o ya no está disponible.
         </p>
         <Image
@@ -21,7 +21,7 @@ export default function NotFoundSection() {
         ></Image>
       </header>
       <footer className=" flex mt-8 flex-col items-center gap-8">
-        <h2 className="text-center w-3/4">Acorta una nueva URL en dos clics</h2>
+        <h2 className="text-center ">Acorta una nueva URL en dos clics</h2>
         <Link href="/">
           <PrimaryButton title="Acortar nueva URL" disabled={false} />
         </Link>
