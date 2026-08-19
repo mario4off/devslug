551517972d37687916c0145e4c6c39f9cf861389
mario4off <mario31991@gmail.com>
2026-08-19 15:10:05 +0200

    feat: add custom headers to fetch request in extractMetaDataFromUrl function

--- services/metada.service.ts
+++ services/metada.service.ts
@@ -2,7 +2,15 @@ import * as cheerio from "cheerio";
 import prisma from "@/lib/db";
 
 export async function extractMetaDataFromUrl(url: string) {
-  const response = await fetch(url);
+  const response = await fetch(url, {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
+      Accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+      "Accept-Language": "es-ES,es;q=0.9",
+    },
+  });
 
   console.log("STATUS:", response.status);
   console.log("OK:", response.ok);
