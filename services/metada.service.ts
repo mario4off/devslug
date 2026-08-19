753e707d179fb9db09e6114d5f90944d0be001e9
mario4off <mario31991@gmail.com>
2026-08-19 15:13:31 +0200

    refactor: simplify fetch logic in extractMetaDataFromUrl function

--- services/metada.service.ts
+++ services/metada.service.ts
@@ -2,25 +2,7 @@ import * as cheerio from "cheerio";
 import prisma from "@/lib/db";
 
 export async function extractMetaDataFromUrl(url: string) {
-  const response = await fetch(url, {
-    headers: {
-      "User-Agent":
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
-      Accept:
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-      "Accept-Language": "es-ES,es;q=0.9",
-    },
-  });
-
-  console.log("STATUS:", response.status);
-  console.log("OK:", response.ok);
-  console.log("URL:", response.url);
-  console.log("CONTENT-TYPE:", response.headers.get("content-type"));
-
-  const html = await response.text();
-
-  console.log("HTML LENGTH:", html.length);
-  console.log("HTML START:", html.slice(0, 500));
+  const html = await fetch(url).then((r) => r.text());
 
   const $ = cheerio.load(html);
 
@@ -31,7 +13,7 @@ export async function extractMetaDataFromUrl(url: string) {
     favicon: $('link[rel="icon"]').attr("href") ?? null,
   };
 
-  console.log("estos son los metadata4534: ", metadata);
+  console.log("estos son los metadata: ", metadata);
   return metadata;
 }
 
