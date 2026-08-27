e80a9d1dd4fd1b3cd6ed7d602c54ce346996eba5
mario4off <mario31991@gmail.com>
2026-08-27 16:16:18 +0200

    feat: enhance UrlsTable with sorting and pagination controls, and improve empty state handling

--- components/UrlsTable.tsx
+++ components/UrlsTable.tsx
@@ -3,7 +3,6 @@
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
@@ -12,32 +11,70 @@ import {
 import { Url } from "@/lib/generated/prisma/client";
 
 import {
+  ArrowDown,
+  ArrowUp,
+  ArrowUpDown,
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+  Search,
+} from "lucide-react";
+
+import {
+  rowSortingFeature,
+  createSortedRowModel,
+  sortFn_alphanumeric,
+  sortFn_text,
   useTable,
   tableFeatures,
   rowPaginationFeature,
   createPaginatedRowModel,
+  createColumnHelper,
 } from "@tanstack/react-table";
 import { use } from "react";
 
 const features = tableFeatures({
+  rowSortingFeature,
   rowPaginationFeature,
+  sortedRowModel: createSortedRowModel(),
   paginatedRowModel: createPaginatedRowModel(),
+  sortFns: {
+    alphanumeric: sortFn_alphanumeric,
+    text: sortFn_text,
+  },
 });
 
-const columns = [
-  {
-    accessorKey: "originalUrl",
+const columnHelper = createColumnHelper<typeof features, Url>();
+const columns = columnHelper.columns([
+  columnHelper.accessor("originalUrl", {
     header: "URL Original",
-  },
-  {
-    accessorKey: "slug",
+    cell: (info) => (
+      <p className="truncate" title={info.getValue()}>
+        {info.getValue()}
+      </p>
+    ),
+  }),
+  columnHelper.accessor("slug", {
     header: "Slug",
-  },
-  {
-    accessorKey: "createdAt",
+    cell: (info) => (
+      <p
+        className="rounded bg-teal-500/10 w-fit px-2 py-1 text-sm text-teal-400"
+        title={info.getValue()}
+      >
+        {info.getValue()}
+      </p>
+    ),
+  }),
+  columnHelper.accessor("createdAt", {
     header: "Fecha de Creación",
-  },
-];
+    cell: (info) => (
+      <code className=" px-2 py-1 text-sm">
+        {info.getValue().toLocaleDateString("es-ES")}
+      </code>
+    ),
+  }),
+]);
 
 export default function UrlsTable({
   urlsPromise,
@@ -49,7 +86,7 @@ export default function UrlsTable({
     features,
     columns,
     data: urls,
-
+    enableSortingRemoval: false,
     initialState: {
       pagination: {
         pageIndex: 0,
@@ -65,45 +102,69 @@ export default function UrlsTable({
         <TableHeader>
           {table.getHeaderGroups().map((group) => (
             <TableRow key={group.id}>
-              {group.headers.map((header) => (
-                <TableHead
-                  className="bg-white/5 px-4 py-3 text-xs font-semibold uppercase tracking-wide text-zinc-400"
-                  key={header.id}
-                >
-                  {header.isPlaceholder ? null : (
-                    <table.FlexRender header={header} />
-                  )}
-                </TableHead>
-              ))}
+              {group.headers.map((header) => {
+                const sorted = header.column.getIsSorted();
+                const SortIcon =
+                  sorted === "asc"
+                    ? ArrowUp
+                    : sorted == "desc"
+                      ? ArrowDown
+                      : ArrowUpDown;
+
+                return (
+                  <TableHead
+                    className="bg-white/5 px-4 py-3 text-xs font-semibold uppercase tracking-wide text-zinc-400"
+                    key={header.id}
+                  >
+                    {header.isPlaceholder ? null : (
+                      <button
+                        type="button"
+                        className="flex w-full items-center gap-2 text-left hover:text-white transition duration-200 ease-in-out"
+                        onClick={header.column.getToggleSortingHandler()}
+                      >
+                        <table.FlexRender header={header} />
+                        <SortIcon className="" />
+                      </button>
+                    )}
+                  </TableHead>
+                );
+              })}
             </TableRow>
           ))}
         </TableHeader>
         <TableBody>
-          {table.getRowModel().rows.map((row) => {
-            const url = row.original;
-
-            return (
-              <TableRow className="h-12 border-white/10" key={url.id}>
-                <TableCell className="max-w-[250px] px-4 py-3 text-zinc-300">
-                  <p className="truncate" title={url.originalUrl}>
-                    {url.originalUrl}
-                  </p>
-                </TableCell>
-                <TableCell className="px-4 py-3">
-                  <code className="rounded bg-teal-500/10 px-2 py-1 text-sm text-teal-400">
-                    {url.slug}
-                  </code>
-                </TableCell>
-                <TableCell className="px-4 py-3 text-zinc-400">
-                  {url.createdAt.toLocaleDateString("es-ES")}
-                </TableCell>
+          {table.getRowModel().rows.length === 0 ? (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No tienes URLs acortadas todavía.
+              </TableCell>
+            </TableRow>
+          ) : (
+            table.getRowModel().rows.map((row) => (
+              <TableRow className="h-12 border-white/10" key={row.id}>
+                {row.getAllCells().map((cell) => (
+                  <TableCell
+                    className=" max-w-[250px] px-4 py-3 text-zinc-300"
+                    key={cell.id}
+                  >
+                    <table.FlexRender cell={cell} />
+                  </TableCell>
+                ))}
               </TableRow>
-            );
-          })}
+            ))
+          )}
         </TableBody>
       </Table>
-      <div className="px-8 mt-6 flex flex-col items-center justify-between ">
-        <div className="flex gap-12">
+      <div className="px-8 mt-7 flex justify-center ">
+        <div className="flex gap-12 items-center">
+          <button
+            className="flex items-center ga-2 disabled:opacity-40"
+            onClick={() => table.firstPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <ChevronsLeft color="#ffff" />
+            <span className=" sr-only">Ir a la primera página</span>
+          </button>
           <button
             className="text-white disabled:opacity-40"
             onClick={() => table.previousPage()}
@@ -121,6 +182,14 @@ export default function UrlsTable({
             disabled={!table.getCanNextPage()}
           >
             Siguiente
+          </button>
+          <button
+            className=" flex items-center gap-2 disabled:opacity-40"
+            onClick={() => table.lastPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            <span className="sr-only">Ir a la última página</span>
+            <ChevronsRight color="#ffff" />
           </button>
         </div>
       </div>
