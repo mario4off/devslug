de611977d8c59a278f15572f484a15de56adcf55
mario4off <mario31991@gmail.com>
2026-09-03 10:26:26 +0200

    fix: adjust padding and spacing for consistency across components

--- components/ui/field-error.tsx
+++ components/ui/field-error.tsx
@@ -1,7 +1,5 @@
 export default function FieldError({ error }: { error: String[] }) {
   if (!error?.length) return;
 
-  return (
-    <p className="text-red-500 mb-1 text-sm p-3 text-center">{error[0]}</p>
-  );
+  return <p className="text-red-500 text-sm p-1 text-center">{error[0]}</p>;
 }
